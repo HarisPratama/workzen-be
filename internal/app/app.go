443d55c13fcd9e37d5a56cad4043c3c4e9e3b8ce
HarisPratama <hpratama324@gmail.com>
2026-03-27 13:50:40 +0700

    fix: allow origin

--- internal/app/app.go
+++ internal/app/app.go
@@ -110,7 +110,7 @@ func RunServer() {
 
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000",
+		AllowOrigins:     "http://localhost:3000, https://workzen.web.id",
 		AllowCredentials: true,
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
